6daeb82109f2a38c155a13f63cfea9bb3b6c0582
sanjaikumar263 <sanjaikumar260301@gmail.com>
2026-09-21 22:40:45 +0530

    Remove the local

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || `${BACKEND_URL}/api`;
